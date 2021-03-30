c14c4a18b910c809a9ee1ff3f2b9ce473edc94a4
Jacob Misirian <misirianrule@gmail.com>
2021-03-29 19:26:18 -0500

    Did some work on Board GUI

--- AICheckers/AICheckers/Board.Designer.cs
+++ AICheckers/AICheckers/Board.Designer.cs
@@ -24,10 +24,17 @@ namespace AICheckers {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Board";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- AICheckers/AICheckers/Board.cs
+++ AICheckers/AICheckers/Board.cs
@@ -9,8 +9,56 @@ using System.Windows.Forms;
 
 namespace AICheckers {
     public partial class Board : Form {
+        private const string BLACK_IMAGE = "~/fakePath/black.png";
+        private const string WHITE_IMAGE = "~/fakePath/white.png";
+        private const string EMPTY_IMAGE = "~/fakePath/empty.png";
+
+        private PictureBox[] pictureBoxes;
+        private Game game;
         public Board() {
             InitializeComponent();
+            pictureBoxes = new PictureBox[64];
+
+            int width = 40;
+            int height = 40;
+            int distance = 5;
+            int startX = 10;
+            int startY = 10;
+
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Left = startX + (x * height + distance);
+                    pictureBox.Top = startY + (y * width + distance);
+                    pictureBox.Width = width;
+                    pictureBox.Height = height;
+                    int tmpX = x;
+                    int tmpY = y;
+                    pictureBox.Click += (s, e) => { selectSquare(tmpX+1, tmpY+1); };
+
+                    Controls.Add(pictureBox);
+                    pictureBoxes[y * 8 + x] = pictureBox;
+                }
+            }
+
+            // TODO: populate pictureBoxes.
+            game = new Game();
+            refreshBoxes();
+        }
+
+        private void selectSquare(int x, int y) {
+            MessageBox.Show(string.Format("You just clicked ({0}, {1})!", x, y));
+        }
+
+        private void refreshBoxes() {
+            for (int i = 0; i < pictureBoxes.Length; i++) {
+                if (game.Current.Tiles[i] == Node.BLACK)
+                    pictureBoxes[i].ImageLocation = BLACK_IMAGE;
+                else if (game.Current.Tiles[i] == Node.WHITE)
+                    pictureBoxes[i].ImageLocation = WHITE_IMAGE;
+                else
+                    pictureBoxes[i].ImageLocation = EMPTY_IMAGE;
+            }
         }
     }
 }
